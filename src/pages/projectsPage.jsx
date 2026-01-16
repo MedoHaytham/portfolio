44d9ef53a8a65f578c1768dc069e89539650d6c1
MedoHaytham <104214885+MedoHaytham@users.noreply.github.com>
2026-01-16 23:26:07 +0200

    add two projects

--- src/pages/projectsPage.jsx
+++ src/pages/projectsPage.jsx
@@ -17,19 +17,33 @@ const projectsData = [
     github: 'https://github.com/MedoHaytham/prayer-times',
     site: 'https://medohaytham.github.io/prayer-times/'
   },
-    {
+  {
     id: 3,
     image:'project 3.jpg',
     title: 'admin Page',
     github: 'https://github.com/MedoHaytham/admin-dashboard',
     site: 'https://admin-dashboard-black-nine-79.vercel.app'
   },
-    {
+  {
     id: 4,
     image:'project 4.jpg',
     title: 'Weather App',
     github: 'https://github.com/MedoHaytham/weather-app',
     site: 'https://weather-app-theta-nine-f575urx091.vercel.app'
+  },
+  {
+    id: 5,
+    image:'project 5.jpg',
+    title: 'Wordle Game',
+    github: 'https://github.com/MedoHaytham/wordle-game',
+    site: 'https://medohaytham.github.io/wordle-game/'
+  },
+  {
+    id: 6,
+    image:'project 6.jpg',
+    title: 'Hangman Game',
+    github: 'https://github.com/MedoHaytham/Hangman-Game',
+    site: 'https://medohaytham.github.io/Hangman-Game/'
   },
 ]
 
